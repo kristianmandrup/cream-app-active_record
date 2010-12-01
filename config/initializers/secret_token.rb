a407840e161c4ec6f62405c300c0128a87cc0cf6
Kristian Mandrup <kmandrup@gmail.com>
2010-12-01 21:11:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreamAppActiveRecord::Application.config.secret_token = 'ef25899b46b7375f1d04d66da62879bd536803aaec93c752b48a73c5f6693702991ebb54206be96de20acc52edc2cb9c13c68280d161a7be541c416be5f7ed08'
